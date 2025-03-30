de1d0edf644a0b1fd88d25a44041576197b6b0f9
suppur <jmahour@hotmail.com>
2025-03-30 01:04:08 -0400

    added bad word validation

--- handlers.go
+++ handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -35,49 +33,4 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
-}
-
-func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type returnVals struct {
-		Valid bool   `json:"valid"`
-		Error string `json:"error"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		errP := returnVals{
-			Error: fmt.Sprintf("Something went wrong %s", err),
-		}
-		log.Print(errP.Error)
-		w.WriteHeader(400)
-		return
-	}
-
-	if len(params.Body) > 140 {
-		errP := returnVals{
-			Error: "Chirp is too long",
-		}
-		log.Print(errP.Error)
-		w.WriteHeader(400)
-		return
-	}
-
-	respBody := returnVals{
-		Valid: true,
-	}
-	data, err := json.Marshal(respBody)
-	if err != nil {
-		log.Printf("Error marshalling response body: %s", err)
-		w.WriteHeader(400)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write(data)
 }
