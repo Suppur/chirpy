d8a3119c211189c07804dbbb18a6b9e127113625
suppur <zi213@hotmail.com>
2025-04-12 16:10:55 -0400

    added auth package for pw hashing and checking

--- internal/auth/password.go
+++ internal/auth/password.go
@@ -1,15 +1,32 @@
 package auth
 
 import (
+	"encoding/hex"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) (string, err error) {
-	hashedPW, err := bcrypt.GenerateFromPassword(password)
+func HashPassword(password string) (string, error) {
+	cost, err := bcrypt.Cost([]byte(password))
+	if err != nil {
+		log.Printf("error retrieving hashing cost: %s", err)
+	}
+	hashedPW, err := bcrypt.GenerateFromPassword([]byte(password), cost)
 	if err != nil {
 		log.Print(err)
 	}
-	return hashedPW, nil
+	return hex.EncodeToString(hashedPW[:]), nil
+}
+
+func CheckPasswordHash(hash, password string) error {
+	decHash, err := hex.DecodeString(hash)
+	if err != nil {
+		log.Printf("error decoding hash: %s", err)
+	}
+	ok := bcrypt.CompareHashAndPassword(decHash, []byte(password))
+	if ok != nil {
+		log.Printf("error, incorrect password: %s", err)
+	}
+	return nil
 }
---
+++ internal/auth/password_test.go
@@ -0,0 +1,5 @@
+package auth
+
+import "testing"
+
+func TestPasswordHashing(t *testing.T) {}
